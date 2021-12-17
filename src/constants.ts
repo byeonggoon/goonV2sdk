7c3c3c57028004308431d3344bbfa0fa498659c9
byeonggoon <77770252+byeonggoon@users.noreply.github.com>
2021-12-17 17:52:25 +0900

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+import JSBI from 'jsbi'
+
+// rinkeby 0xd68f7F2FE64FEd58Cf35571e52BB330d342EfF9E
+export const FACTORY_ADDRESS = '0xd68f7F2FE64FEd58Cf35571e52BB330d342EfF9E'
+
+//hi hashcode  0x2a710137d93451853521eea4f7d29ac8c3b69b0054825c667c9061bc85d79616
+export const INIT_CODE_HASH = '0x2a710137d93451853521eea4f7d29ac8c3b69b0054825c667c9061bc85d79616'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
